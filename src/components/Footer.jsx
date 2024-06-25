d6b99e83bc377a16c3a44f031a6073502b953000
Lucas <lukefranc3@yahoo.com>
2024-06-25 14:24:22 -0400

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer(props) {
     <section id="footer-section">
       <div className="container">
         <p>&copy; Lucas Franco 2023</p>
-        <Link className="nav-link" to="/">{props.parent==='home' ? 'Attendant Login' : 'Customer Page'}</Link>
+        <Link className="nav-link" to="/login">{props.parent==='home' ? 'Attendant Login' : 'Customer Page'}</Link>
       </div>
     </section>
   );
